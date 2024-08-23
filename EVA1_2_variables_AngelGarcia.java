0b7a857878782d907e06d8de5f44817003980d05
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-23 17:54:03 -0600

    Add files via upload

---
+++ EVA1_2_variables_AngelGarcia.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables_angel.garcia;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_variables_AngelGarcia {
+
+                                    // TIPOS DE DATOS
+    public static void main(String[] args) {
+System.out.println("HOLA MUNDO!!"); // Cadena de texto (nombre, numero de control)
+System.out.println(100);            //entero (edad)
+System.out.println('c');            // caracter (sexo)
+System.out.println(true);           //valores logicos(booleanos)
+System.out.println(3.1416);         // numero flotante  (salario)
+System.out.println
+        (
+        false
+        )
+        ;               // ; fin de instrucción (escencial )
+    }
+    
+    
+    // ENTEROS:
+    // int 
+    // long
+    // short
+    
+    // NUMEROS CON PARTE DECIMAL:
+    // float
+    // double
+    
+    // CADENAS DE TEXTO:
+    // String
+    
+    // CARACTERES:
+    // char
+    
+    // VALORES LOGICOS:
+    // Boolean
+    
+    // DECLARACIÓN DE VARIABLES:
+    // Sintaxis para declarar variables (REGLAS)
+    // Tipo de dato nombre de la variable 
+    
+    String nombre;
+    String apellido;
+    String curp;
+    float promedio;
+    
+    
+    
+}
