89229a5387737d78ad4b3de413f843e637ab480c
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-26 17:53:53 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_variables2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // vehiculos
+        String marca;
+        
+        String modelo;
+        
+        int year;    // la ñ no es aceptada en otros lenguajes 
+        
+        double precio;
+        
+        // ASIGNANDO 
+        
+        marca = "NISSAN";
+        
+        modelo = "GTR";
+        
+        year = 2008;
+        
+        precio = 1000000.1;
+        
+               
+        
+   
+        System.out.println(marca);
+        
+        System.out.println(modelo);
+        
+        System.out.println(year);
+        
+        System.out.println(precio);
+        
+        
+    
+}
+}
