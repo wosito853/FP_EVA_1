eb3a5b7c8c99515158f4a594876fffd755133a56
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-26 21:34:56 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO_AIGS/src/eva1_1_hola_mundo_aigs/EVA1_1_HOLA_MUNDO_AIGS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo_aigs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_1_HOLA_MUNDO_AIGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("HOLA MUNDO !!!");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
