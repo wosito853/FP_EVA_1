effe4a039619b39b04d8b7f3e35da1a065d3d356
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-26 21:38:03 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES_AIGS/src/eva1_2_variables_aigs/EVA1_2_VARIABLES_AIGS.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variables_aigs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_VARIABLES_AIGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+                                    // TIPOS DE DATOS
+  
+System.out.println("HOLA MUNDO!!"); // Cadena de texto (nombre, numero de control)
+System.out.println(100);            //entero (edad)
+System.out.println('c');            // caracter (sexo)
+System.out.println(true);           //valores logicos(booleanos)
+System.out.println(3.1416);         // numero flotante  (salario)
+System.out.println
+        (
+        false
+        )
+        ;               // ; fin de instrucción (escencial )
+    
+    
+    
+    // ENTEROS:
+    // int 
+    // long
+    // short
+    
+    // NUMEROS CON PARTE DECIMAL:
+    // float
+    // double
+    
+    // CADENAS DE TEXTO:
+    // String
+    
+    // CARACTERES:
+    // char
+    
+    // VALORES LOGICOS:
+    // Boolean
+    
+    // DECLARACIÓN DE VARIABLES:
+    // Sintaxis para declarar variables (REGLAS)
+    // Tipo de dato nombre de la variable 
+    
+    String nombre;
+    String apellido;
+    String curp;
+    double promedio;
+    int edad; 
+    edad = 18;  // ASIGNACIÓN; Estoy colocando el valor 18 
+              // dentro de la variable
+    
+   nombre = "Angel Garcia";
+   
+   apellido = "Garcia Soto";
+   
+   promedio = 8.8;
+   
+   
+   System.out.println(nombre);
+   System.out.println(edad);
+   System.out.println(apellido);
+   System.out.println(promedio);
+   
+   
+   
+    }
+    
+}
