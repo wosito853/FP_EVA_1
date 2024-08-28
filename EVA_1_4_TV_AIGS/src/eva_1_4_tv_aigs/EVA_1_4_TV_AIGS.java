039e2938d690b0ada75f98ec88c19c75abec4b97
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-28 17:24:58 -0600

    Add files via upload

---
+++ EVA_1_4_TV_AIGS/src/eva_1_4_tv_aigs/EVA_1_4_TV_AIGS.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_4_tv_aigs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_4_TV_AIGS {
+
+    private static String Sony;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+      String marca;
+        String modelo;
+        String Tamaño;
+        int controles;
+        double precio;
+        String tecnologia;
+      
+      
+    marca = "samsung";
+    modelo = "CU8000B";
+    Tamaño = "65 pulgadas";
+    controles = 2;
+    precio = 12999.31;
+    tecnologia = "Crystal UHD";
+    
+    
+    System.out.println(marca);
+    System.out.println(modelo);
+    System.out.println(Tamaño);
+    System.out.println(controles);
+    System.out.println(precio);
+    System.out.println(tecnologia);
+    
+            
+
+
+
+
+    }
+    
+}
