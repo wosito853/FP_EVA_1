ec71ea15558f34e0bfbf8d778a0c9f8275406f0b
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-29 17:51:09 -0600

    Add files via upload

---
+++ EVA1_7_CAPTURA_AUTO_AIGS/src/eva1_7_captura_auto_aigs/EVA1_7_CAPTURA_AUTO_AIGS.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_captura_auto_aigs;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_7_CAPTURA_AUTO_AIGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String marca;
+        String modelo;
+        int year; 
+        double precio;
+        
+        Scanner capturar;
+        capturar = new Scanner(System.in);
+        
+        System.out.println("escribe la marca de carro");
+        marca = capturar.nextLine();
+        
+        System.out.println("escribe tu modelo de carro");
+        modelo = capturar.nextLine();
+        
+        System.out.println("introduce el año");
+        year = capturar.nextInt();
+        
+        System.out.println("introduce el precio");
+        precio = capturar.nextDouble();
+        
+        
+        
+        
+        
+     
+        
+        
+        
+        
+        
+    }
+    
+}
