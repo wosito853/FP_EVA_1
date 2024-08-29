f69bace931625f973760b07b116aa3419a2e0254
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-08-29 17:37:49 -0600

    Add files via upload

---
+++ EVA_1_6_ENTRADA_DATOS2/src/eva_1_6_entrada_datos2/EVA_1_6_ENTRADA_DATOS2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_6_entrada_datos2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_6_ENTRADA_DATOS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
