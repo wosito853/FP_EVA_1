3511ae0440f8080cd34d3f31618ba391a2b2a798
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-09-03 17:47:59 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES_AIGS/src/eva1_9_operaciones_aigs/EVA1_9_OPERACIONES_AIGS.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_aigs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_9_OPERACIONES_AIGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Las operaciones dependen del tpo de dagos 
+        // numeros enteros y decimales:   
+        //suma, resta mult, división, pot, raíces, etc ;
+        // precedencia de operaciones;
+        // 1 potencias y raíces
+        // 2 mul y división
+        // 3 sumas y restas
+        // operaciones iguales--> izq a derecha
+        // 3*5 / 2
+        // boolean (valores lógicos --> true o false)
+        // mate discretas--> lógica booleana
+        //cadenas ( string)
+        // concatenación --> unir cadenas de texto
+        
+        
+        
+        
+        
+        
+        
+        
+        // OPERACIONES ARITMETICAS
+        // suma y resta
+        // inicialización de una varible 
+        int x = 10; // asignción y declaración 
+        int y = 5; 
+        // si necesitan muchas variables 
+        int suma, resta, mult, div, pot, raíz;
+        suma = x+y; //operador de suma (+)
+        System.out.println("suma de x + y");
+         System.out.println(suma);
+        resta = x-y;
+         System.out.println("Resya de x-y");
+        System.out.println(resta);
+        mult = x*y;
+         System.out.println("Multiplicación de x*y");
+        System.out.println(mult);
+       // DIVISIÓN
+       // en jav las operaciones dependen del tipo de datos 
+       // divides entero, java da como resultado entero
+       div = x/y;
+       
+         System.out.println("Division de x(10)/y(5)");
+          System.out.println(div);
+          // diisión con números con punto flotante
+          double va11 = 5;
+          double va12 = 2;
+          double resu;
+          resu = va11/va12;
+           System.out.println("División de va11(5)/ va12(2)");
+            System.out.println(resu);
+          
+         
+       
+        
+    }
+    
+}
