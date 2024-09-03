cb12ff83193657fb9311553961d384bf675870c5
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-09-03 17:48:37 -0600

    Add files via upload

---
+++ EVA1_10_OPERACIONES_2_AIGS/src/eva1_10_operaciones_2_aigs/EVA1_10_OPERACIONES_2_AIGS.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_operaciones_2_aigs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_10_OPERACIONES_2_AIGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // CONCATENACIÓN (CADENAS);
+        // operador (+)
+        // mismo simbolo con diferentes usos:
+        // sobrecarga de operadores 
+        String nombre = "Angel";
+        String apellido = "Garcia";
+        String Nc;
+        // contatenar;unir cadenas de texto
+        Nc = nombre + " " + apellido;
+         
+        
+         
+         // imprimir datos del uuario;
+          System.out.println("nombre del usuario; " + nombre);
+           System.out.println("apellido del usuario; " + apellido );
+            System.out.println("Nombre Completo del usario; " + Nc);
+        
+    }
+    
+}
