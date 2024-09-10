ea2b6c5a8b0e08504efcf337d43871694e362920
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-09-10 17:32:12 -0600

    Add files via upload

---
+++ EVA1_15_AGUIELA_SELLO/src/eva1_15_aguiela_sello/EVA1_15_AGUIELA_SELLO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_aguiela_sello;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_15_AGUIELA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // mayores que 0.0
+        // menores que1.0
+       // 0.0 -- 0.99999
+        double valor = (Math.random());
+        
+        
+        if(valor < 0.5){
+            System.out.println("aguila");
+        }else {
+            System.out.println("sello");
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
